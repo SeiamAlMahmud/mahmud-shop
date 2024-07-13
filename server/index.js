9c9ece179dcd1ba55607b7d813a5a808d4584f95
SeiamAlMahmud <samahmud49@gmail.com>
2024-07-13 04:51:00 +0000

    node cached

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 const bodyParser = require('body-parser');
 const express = require('express')
 const app = express()
+const compression = require('compression')
 const multer = require('multer');
+const NodeCache = require('node-cache');
+const cache = new NodeCache({ stdTTL: 600 }); // Cache for 10 minutes
+
 const path = require('path');
 const fs = require('fs');
 const cors = require('cors')
@@ -9,6 +13,16 @@ require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 3000
 
+app.use(compression({
+  level: 6,
+  threshold: 10 * 1000,
+  filter: (req, res) => {
+    if (req.header['x-no-compression']) {
+      return false
+    }
+  }
+
+}))
 
 app.use(bodyParser.json())
 const corsOptions = {
@@ -49,15 +63,18 @@ async function run() {
         cb(null, Date.now() + '-' + path.extname(file.originalname));
       }
     });
-    
-    const upload = multer({ storage: storage });
-    
+
+    const upload = multer({
+      storage
+
+    });
+
     // get all users
     app.get('/users', async (req, res) => {
-      
+
       console.log(req.query.email)
       if (req.query.email) {
-        const result = await usersCollection.find({email: req.query.email}).toArray();
+        const result = await usersCollection.find({ email: req.query.email }).toArray();
         res.send(result)
 
       }
@@ -67,31 +84,39 @@ async function run() {
 
       }
     })
-    app.post('/addProduct', upload.single('image'), async (req,res) => {
+    app.post('/addProduct', upload.single('image'), async (req, res) => {
       if (!req.file) {
         return res.status(400).send('No file uploaded.');
       }
       console.log(req.body)
       try {
-    const img = fs.readFileSync(path.join(__dirname, 'uploads', req.file.filename))
-   
-    const encode_image = img.toString('base64');
+        const img = fs.readFileSync(path.join(__dirname, 'uploads', req.file.filename))
 
-    const finalImg = {
-      contentType: req.file.mimetype,
-      img: encode_image,
-      name: req.body.name,
-      price: req.body.price,
-      quantity: req.body.quantity,
-      description: req.body.description,
-  };
+        const encode_image = img.toString('base64');
 
-  const result = await productsCollection.insertOne(finalImg);
-            res.status(200).json({ id: result.insertedId });
-      
-      // const { name, price, quantity ,description} = req.body;
-      // const imageUrl = `http://127.0.0.1:3000/uploads/${req.file.filename}`;
-    
+        const finalImg = {
+          contentType: req.file.mimetype,
+          img: encode_image,
+          name: req.body.name,
+          price: req.body.price,
+          quantity: req.body.quantity,
+          description: req.body.description,
+        };
+
+        const result = await productsCollection.insertOne(finalImg);
+        res.status(200).json({
+          success: true,
+          id: result.insertedId
+        });
+        // Delete the image file after saving to MongoDB
+        fs.unlink(path.join(__dirname, 'uploads', req.file.filename), (err) => {
+          if (err) {
+            console.error('Error deleting file:', err);
+          }
+        })
+        // const { name, price, quantity ,description} = req.body;
+        // const imageUrl = `http://127.0.0.1:3000/uploads/${req.file.filename}`;
+
         // const result =await productsCollection.insertOne(user)
         // const result = await productsCollection.insertOne({
         //   name,
@@ -100,52 +125,88 @@ async function run() {
         //   imageUrl,
         //   description
         // });
-    
+
         // res.status(201).json({ message: 'Product added successfully', id: result.insertedId });
       } catch (error) {
         console.error('Error saving to database:', error);
         res.status(500).send('Error saving to database');
-      } 
-     
+      }
+
 
     }
     )
-    app.post('/addUser', async (req,res) => {
+    app.post('/addUser', async (req, res) => {
       const user = req.body
       // console.log(user)
-      const isAvailable = await usersCollection.find({email: user.email}).toArray()
-      console.log(isAvailable)
+      const isAvailable = await usersCollection.find({ email: user.email }).toArray()
+      // console.log(isAvailable)
       if (isAvailable.length == 0) {
-        
+
         const result = await usersCollection.insertOne(user)
         console.log(result)
-        res.json(result[0])
+        // res.json(result[0])
       }
       if (isAvailable.length !== 0) {
         res.json(isAvailable[0])
       }
     })
-    
 
 
-   
-// get all data from database
-    app.get('/products', async (req,res)=> {
-      const result = await productsCollection.find({}).toArray();
-      // console.log(result)
-      res.json(result)
+
+
+    // get all data from database
+    app.get('/products', async (req, res) => {
+      try {
+        console.log(req.query.page)
+        const limit = req.query.limit || 10
+        const page = req.query.page || 1
+        const skipAmount = (page - 1) * limit;
+  
+
+        const cacheKey = `products_${page}_${limit}`;
+  
+        const cachedData = cache.get(cacheKey);
+        if (cachedData) {
+          return res.json(cachedData);
+        }
+      
+
+        const products = await productsCollection.find({})
+    .skip(skipAmount)
+    .limit(limit)
+    .project({
+      _id: 1,     
+       id: 1,
+      name: 1,
+      rating: 1,
+      seller: 1,
+      stock: 1,
+      img: 1,
+      quantity: 1,
+      description: 1
+    })
+    .toArray();
+
+    cache.set(cacheKey, products);
+  res.json(products);
+
+      
+
+      } catch (error) {
+
+      }
     })
 
     //get single data from database
-    app.get('/product/:key', async (req,res) => {
-      console.log(req.params.key)
-      const result = await productsCollection.find({id: req.params.key}).toArray();
+    app.get('/product/:key', async (req, res) => {
+      // console.log(req.params.key)
+      const result = await productsCollection.find({ id: req.params.key }).toArray();
       res.json(result[0])
     })
-    app.get('/productbyid', async (req,res) => {
+    app.get('/productbyid', async (req, res) => {
       console.log(req.query.id)
-      
-      const result = await productsCollection.find({_id:  new ObjectId(req.query.id)}).toArray();
+
+      const result = await productsCollection.find({ _id: new ObjectId(req.query.id) }).toArray();
       res.send(result)
     })
   } finally {
